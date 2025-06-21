bb80d8e8af66ae7e481480671787d110652d790c
hoangminh5210119 <dohoangminh006@gmail.com>
2025-06-21 15:33:52 +0700

    update disable auto check md5 and add for vi

--- TTS/tts/layers/xtts/tokenizer.py
+++ TTS/tts/layers/xtts/tokenizer.py
@@ -425,6 +425,19 @@ _symbols_multilingual = {
             ("°", " 도 "),
         ]
     ],
+    "vi": [
+        # Vietnamese
+        (re.compile(r"%s" % re.escape(x[0]), re.IGNORECASE), x[1])
+        for x in [
+            ("&", " và "),
+            ("@", " tại "),
+            ("%", " phần trăm "),
+            ("#", " số "),
+            ("$", " đô la "),
+            ("£", " bảng Anh "),
+            ("°", " độ "),
+        ]
+    ],
 }
 
 
@@ -450,6 +463,7 @@ _ordinal_re = {
     "tr": re.compile(r"([0-9]+)(\.|inci|nci|uncu|üncü|\.)"),
     "hu": re.compile(r"([0-9]+)(\.|adik|edik|odik|edik|ödik|ödike|ik)"),
     "ko": re.compile(r"([0-9]+)(번째|번|차|째)"),
+    "vi": re.compile(r"([0-9]+)(th|st|nd|rd)"),
 }
 _number_re = re.compile(r"[0-9]+")
 _currency_re = {
@@ -501,6 +515,7 @@ def _expand_currency(m, lang="en", currency="USD"):
         "tr": ", ",
         "hu": ", ",
         "ko": ", ",
+        "vi": ", ",
     }
 
     if amount.is_integer():
@@ -611,6 +626,7 @@ class VoiceBpeTokenizer:
             "ja": 71,
             "hu": 224,
             "ko": 95,
+            "vi": 250,
         }
 
     @cached_property
@@ -637,6 +653,9 @@ class VoiceBpeTokenizer:
         elif lang == "ja":
             txt = japanese_cleaners(txt, self.katsu)
         elif lang == "hi":
+            # @manmay will implement this
+            txt = basic_cleaners(txt)
+        elif lang == "vi":
             # @manmay will implement this
             txt = basic_cleaners(txt)
         else:
